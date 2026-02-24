0cce8243c8a25513ba95179d83cf4db9a0b3502a
MarioRam0s <marioramosmejia2243@gmail.com>
2026-02-23 22:12:18 -0600

    feat: Implementando Clean Architecture y creacion de componentes base

---
+++ src/app/presentation/pages/ubications/ubications.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ubications',
+  imports: [],
+  templateUrl: './ubications.html',
+  styleUrl: './ubications.css',
+})
+export class Ubications {
+
+}
